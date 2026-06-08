233973657ad643f757928828a0cac5019ff37fe8
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2026-06-08 15:45:44 +0700

    feat(prisma): update schema and helpers

---
+++ packages/prisma/helpers.js
@@ -0,0 +1,6 @@
+/**
+ * Bộ lọc bản ghi chưa bị xóa mềm dùng chung
+ */
+export const notDeleted = {
+  deleted_at: null
+};
--- packages/prisma/index.js
+++ packages/prisma/index.js
@@ -2,4 +2,5 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 export { Prisma } from '@prisma/client';
+export * from './helpers.js';
 
